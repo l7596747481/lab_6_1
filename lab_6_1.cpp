9f3d053112116bd7d52c5af8ca6cada8e338b363
l7596747481 <90904700+l7596747481@users.noreply.github.com>
2022-05-27 12:54:44 +0300

    Add files via upload

---
+++ lab_6_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "Array.h"
+#include <time.h>
+
+Array returnDynamicArray(int arr[], const int size)
+{
+    Array dynamicArray = Array(size);
+
+    for (int i = 0; i < size; i++)
+        dynamicArray[i] = arr[i];
+
+    return dynamicArray;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int size = 20;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = -50 + rand() % int(50 - (-50) + 1);
+
+    Array taskArray = returnDynamicArray(arr, size);
+    taskArray.Print();
+
+    taskArray.MyTask();
+    taskArray.Print();
+
+    taskArray.push_back(taskArray.Sum());
+    taskArray.push_back(taskArray.ArithmeticMeanAbs());
+    taskArray.Print();
+
+    cin.get();
+    cin.get();
+    return 0;
+}
